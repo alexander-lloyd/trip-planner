ce0207d6b5d10b7ddd5e6a53510a93407ce496f7
harryrbrown <harry.brown16@imperial.ac.uk>
2017-02-05 00:18:59 +0000

    restructured code

--- python/jsonparse.py
+++ python/jsonparse.py
@@ -3,11 +3,33 @@ import urllib.request
 from pprint import pprint
 import datetime
 from datetime import timedelta
+import requests
 
-KEY = "AIzaSyDbcizcLuZ3ZT1M5xNE3ieYeT2YGn6HJ5s"
+KEY = "AIzaSyDKr3_tYnPHF0zbO08plIyJTE8OkBOHf3w"
 TIME_SPENT_AT_ATTRACTIONS = 2
 
+
+def jsonParseURL(url):
+    # TODO - checks when API key is expired
+    
+    response = requests.get(url)
+    data = response.text
+
+    jsonfile = json.loads(data)
+
+    if jsonfile["status"] == "OVER_QUERY_LIMIT":
+        print("Query limit reached")
+        raise APIQueryLimitReachedException()
+
+
+##    pprint(jsonfile)
+    return (jsonfile)
+
+
 class LocationNotFoundError(Exception):
+    pass
+
+class APIQueryLimitReachedError(Exception):
     pass
 
 class Location:
@@ -16,6 +38,7 @@ class Location:
         self.jsonData = self.getLocationData(location.replace(" ", "+"))
         self.placeName = self.jsonData["result"]["name"]
         self.addressComponents = self.jsonData["result"]["address_components"]
+        self.town = location
         self.placeAddress = self.jsonData["result"]["formatted_address"]
         self.placeLat = self.jsonData["result"]["geometry"]["location"]["lat"]
         self.placeLon = self.jsonData["result"]["geometry"]["location"]["lng"]
@@ -26,22 +49,27 @@ class Location:
             if "postal_town" in self.addressComponents[i]["types"]:
                 self.town = self.addressComponents[i]["long_name"]
                 break
-
+            elif "locality" in self.addressComponents[i]["types"]:
+                self.town = self.addressComponents[i]["long_name"]
+                break
+            
 
     def getLocationData(self, location):
         url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + location + "&key=" + KEY
         try:
-            placeID = self.jsonParseURL(url)["results"][0]["place_id"]
+            placeID = jsonParseURL(url)["results"][0]["place_id"]
         except IndexError:
             raise LocationNotFoundError()
         
         url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeID + "&key=" + KEY
-        return self.jsonParseURL(url)
+        return jsonParseURL(url)
 
         
     def getLocalPlaces(self):
-        url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=things+to+do+near+" + self.town + "&key=" + KEY
-        return self.jsonParseURL(url)
+        url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=popular+attractions+near+" + self.town + "&key=" + KEY
+        url = url.replace(" ", "+")
+
+        return jsonParseURL(url)
 
 
     def printDataOfTopLocation(self):
@@ -49,14 +77,20 @@ class Location:
               + "\nLat: " + str(self.placeLat) + "\nLon: " + str(self.placeLon))
         
 
-    def getDirections(self, otherLocation, time=""):
-        otherPlace = Location(otherLocation)
+
+class Route:
+    def __init__(self, start, end, waypoints=[]):
+        self.start = start
+        self.end = end
+        self.waypoints = waypoints
+
+    def getDirections(self, time=""):
         if time == "":
-            url = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:" + self.placeID + "&destination=place_id:" + otherPlace.placeID + "&mode=transit&key=" + KEY
+            url = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:" + self.start.placeID + "&destination=place_id:" + self.end.placeID + "&mode=transit&key=" + KEY
         else:
-            url = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:" + self.placeID + "&destination=place_id:" + otherPlace.placeID + "&departure_time=" + time + "&mode=transit&key=" + KEY
+            url = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:" + self.start.placeID + "&destination=place_id:" + self.end.placeID + "&departure_time=" + time + "&mode=transit&key=" + KEY
         
-        jsonfile = self.jsonParseURL(url)
+        jsonfile = jsonParseURL(url)
 
         startAddress = jsonfile["routes"][0]["legs"][0]["start_address"]
         endAddress = jsonfile["routes"][0]["legs"][0]["end_address"]
@@ -66,30 +100,26 @@ class Location:
         duration = jsonfile["routes"][0]["legs"][0]["duration"]["value"]
         steps = jsonfile["routes"][0]["legs"][0]["steps"]
 
-        print("From " + self.placeName + " to " + otherPlace.placeName + ":\n")
+        print("From " + self.start.placeName + " to " + self.end.placeName + ":\n")
         for i in range(0, len(steps)):
             if steps[i]["travel_mode"] == "WALKING":
                 print(steps[i]["html_instructions"])
             elif steps[i]["travel_mode"] == "TRANSIT":
-                print("Take the " + steps[i]["transit_details"]["line"]["short_name"] + " line from " +
-                      steps[i]["transit_details"]["departure_stop"]["name"] + " to " +
-                      steps[i]["transit_details"]["arrival_stop"]["name"])
+                try:
+                    print("Take the " + steps[i]["transit_details"]["line"]["short_name"] + " line from " +
+                    steps[i]["transit_details"]["departure_stop"]["name"] + " to " +
+                    steps[i]["transit_details"]["arrival_stop"]["name"])
+                except KeyError:
+                    print("Take the " + steps[i]["transit_details"]["line"]["name"] + " line from " +
+                    steps[i]["transit_details"]["departure_stop"]["name"] + " to " +
+                    steps[i]["transit_details"]["arrival_stop"]["name"])
 
                       
         print(distance + ", departing " + departureTime + " and arriving " +
-              arrivalTime)
+              arrivalTime + "\n\n")
 
         self.tripDuration = duration
-
-
-    def jsonParseURL(self, url):
-        response = urllib.request.urlopen(url)
-        data = response.read()
-        text = data.decode('utf-8')
-
-        jsonfile = json.loads(text)
-        return (jsonfile)
-
+        
 
 
 class DayPlanner:
@@ -99,6 +129,7 @@ class DayPlanner:
 
     def findPlacesToVisit(self, noOfPlaces):
         localPlaces = self.location.getLocalPlaces()["results"]
+##        pprint(localPlaces)
 
         i = 0
         time = datetime.datetime(2017, 2, 6, 9, 0, 0)
@@ -106,7 +137,8 @@ class DayPlanner:
         
         for j in range(0, len(localPlaces)):
             nextLocation = Location(localPlaces[j]["name"])
-            currLocation.getDirections(nextLocation.placeName, str(int(time.timestamp())))
+            route = Route(currLocation, nextLocation)
+            route.getDirections(str(int(time.timestamp())))
             time += timedelta(seconds=currLocation.tripDuration)
             time += timedelta(hours=TIME_SPENT_AT_ATTRACTIONS)
             currLocation = nextLocation
